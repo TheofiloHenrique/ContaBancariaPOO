0366f8a59c1916393ab7b6f052acb62eb43a92fa
Theofilo Henrique <theohenriquee01@gmail.com>
2024-12-31 18:13:59 -0300

    Classe Conta pronta + classe app adicionada

--- src/main/java/entities/Conta.java
+++ src/main/java/entities/Conta.java
@@ -1,5 +1,36 @@
 package entities;
 
 public class Conta {
+    private int numeroConta;
+    private String nomeConta;
+    private double saldoConta;
+
+    public Conta(int numeroConta,String nomeConta) {
+        this.numeroConta = numeroConta;
+        this.nomeConta = nomeConta;
+    }
+
+    public Conta(int numeroConta, String nomeConta, double saldoConta) {
+        this.numeroConta = numeroConta;
+        this.nomeConta = nomeConta;
+        this.saldoConta = saldoConta;
+    }
+
+    public String getNomeConta() {
+        return nomeConta;
+    }
+
+    public void setNomeConta(String nomeConta) {
+        this.nomeConta = nomeConta;
+    }
+
+    public void deposito(double novoDeposito){
+        this.saldoConta += novoDeposito;
+    }
+
+    public void saque(double novoSaque){
+        this.saldoConta -= novoSaque - 5.00;
+    }
+
 
 }
